50c0ae31b59f96de8261be6a3ecce21db756b36d
LiviuSosu <sosuliviu@gmail.com>
2020-10-16 21:22:18 +0200

    Add project files.

---
+++ DotNetCore31MongoDb/Controllers/WeatherForecastController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore31MongoDb.Models;
+using DotNetCore31MongoDb.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace DotNetCore31MongoDb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //https://thecodebuzz.com/mongodb-repository-implementation-unit-testing-net-core-example/
+        private readonly BookService _bookService;
+
+        public WeatherForecastController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Object>> Get() =>
+            _bookService.Get();
+
+        //[HttpGet("{id:length(24)}", Name = "GetBook")]
+        //public ActionResult<Book> Get(string id)
+        //{
+        //    var book = _bookService.Get(id);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return book;
+        //}
+
+        //[HttpPost]
+        //public ActionResult<Book> Create(Book book)
+        //{
+        //    _bookService.Create(book);
+
+        //    return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        //}
+
+        //[HttpPut("{id:length(24)}")]
+        //public IActionResult Update(string id, Book bookIn)
+        //{
+        //    var book = _bookService.Get(id);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _bookService.Update(id, bookIn);
+
+        //    return NoContent();
+        //}
+
+        //[HttpDelete("{id:length(24)}")]
+        //public IActionResult Delete(string id)
+        //{
+        //    var book = _bookService.Get(id);
+
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _bookService.Remove(book.Id);
+
+        //    return NoContent();
+        //}
+    }
+}
---
+++ DotNetCore31MongoDb/Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using DotNetCore31MongoDb.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore31MongoDb.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<object> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<object>("users");
+        }
+
+        public List<Object> Get() =>
+            _books.Find(book => true).ToList();
+
+        //public Book Get(string id) =>
+        //    _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        //public Book Create(Book book)
+        //{
+        //    _books.InsertOne(book);
+        //    return book;
+        //}
+
+        //public void Update(string id, Book bookIn) =>
+        //    _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        //public void Remove(Book bookIn) =>
+        //    _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        //public void Remove(string id) =>
+        //    _books.DeleteOne(book => book.Id == id);
+    }
+}
